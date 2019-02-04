faca7632810a1d53df6a9bf34ff88f13d70b54fd
Isaac Morneau <isaacmorneau@gmail.com>
2019-02-04 02:37:20 -0800

    working v1 to break the ciphers

--- transbreak.py
+++ transbreak.py
@@ -1,48 +1,81 @@
 #!/usr/bin/env python3
 
-import sys, string
+import sys, string, itertools, math
+from multiprocessing import Pool
 
-def key_to_col(keystr):
-    keypos = 1
-    keycol = [0 for i in range(len(keystr))]
-    for v in string.ascii_lowercase:
-        for k,s in enumerate(keystr):
-            if s == v:
-                keycol[k] = keypos
-                keypos += 1
-    keycol = [s[1] for s in sorted((k, i) for i,k in enumerate(keycol))]
-    return keycol, keystr
+def encrypt_message(key, message):
+    ciphertext = [''] * key
+    for col in range(key):
+        pointer = col
+        while pointer < len(message):
+            ciphertext[col] += message[pointer]
+            pointer += key
+    return ''.join (ciphertext)
 
-def encrypt(keycol, plain, cipher):
-    index = 0
-    rows = [[]]
-    with open(plain, "r") as p, open(cipher, "w+") as c:
-        for line in p:
-            for char in line:
-                rows[-1].append(char)
-                index += 1
-                if index == len(keycol):
-                    rows.append([])
-                    index = 0
-        if len(rows[-1]) == 0:
-            del rows[-1]
-        else:
-            for i in range(len(rows[-1]), len(keycol)):
-                rows[-1].append(' ')
-        print(rows)
-        for row in rows:
-            for k in keycol:
-                c.write(row[k])
+
+def decrypt_message(key, message):
+    cols = math.ceil(len(message) / key)
+    rows = key
+    extra = (cols * rows) - len(message)
+    plaintext = [''] * cols
+    row = col = 0
+    for m in message:
+        plaintext[col] += m
+        col += 1
+        if (col == cols) or (col == cols - 1 and row >= rows - extra):
+            col = 0
+            row += 1
+    return ''.join(plaintext)
+
+def trykey(key):
+    #score the key by counting all matching words over 5 characters long
+    return sum(1 for word in decrypt_message(key, cipher).split() if len(word) >= 5 and word in words)
+
+def transbreak(cipherfile, dictfile):
+    global cipher
+    global words
+    cipher = open(cipherfile).read()
+    words = open(dictfile).read().split()
+    try:
+        listing = Pool().imap(trykey, range(1,len(cipher)))
+        best = 0
+        best_index = -1
+        for index, score in enumerate(listing):
+            if score > best:
+                best = score
+                best_index = index
+                print("best so far keylen: {} score: {}".format(index+1, score))
+            if index and index % 100 == 0:
+                print("{:.01f}% {}/{}".format(index/len(cipher)*100, index, len(cipher)))
+    except KeyboardInterrupt:
+        pass
+    return best_index+1, best
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("usage ./transbreak.py encrypt <key> /path/to/plaintext /path/to/ciphertext")
-        print("usage ./transbreak.py decrypt <key> /path/to/plaintext /path/to/ciphertext")
-        print("usage ./transbreak.py break /path/to/ciphertext")
-    if sys.argv[1] == "encrypt":
-        keycol, key = key_to_col(sys.argv[2])
-        print("using key:", keycol)
-        encrypt(keycol, sys.argv[3], sys.argv[4])
-    if sys.argv[1] == "decrypt":
-        key_to_col(sys.argv[2])
+    if len(sys.argv) == 5:
+        if sys.argv[1] == "encrypt":
+            with open(sys.argv[4], "w+") as o:
+                ciphertxt = encrypt_message(int(sys.argv[2]), open(sys.argv[3], "r").read())
+                o.write(ciphertxt)
+                print(ciphertxt)
+        if sys.argv[1] == "decrypt":
+            with open(sys.argv[4], "w+") as o:
+                plaintxt = decrypt_message(int(sys.argv[2]), open(sys.argv[3], "r").read())
+                o.write(plaintxt)
+                print(plaintxt)
+    elif len(sys.argv) == 4:
+        if sys.argv[1] == "break":
+            index, best = transbreak(sys.argv[2], sys.argv[3])
+            if index == -1:
+                print("all cipher lengths equally terrible")
+            else:
+                #print(decrypt_message(index, open(sys.argv[2], "r").read()))
+                print("best cipher length {}".format(index))
+    else:
+        print("usage ./transbreak.py encrypt <key> /path/to/input /path/to/output")
+        print("usage ./transbreak.py decrypt <key> /path/to/input /path/to/output")
+        print("usage ./transbreak.py break /path/to/ciphertext /path/to/dictionary")
+        print("    to generate a dictionary the following can be used:")
+        print("        aspell dump master | grep -v \"'\" -E '.{5,}' > dict")
+        sys.exit(1)
